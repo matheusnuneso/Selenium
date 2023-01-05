ae37d94b3cbf1b78f5a3ed90ec716f413541da9b
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-01-05 17:16:28 -0300

    change the site of the tests

--- src/test/java/modulos/ProductTest.java
+++ src/test/java/modulos/ProductTest.java
@@ -3,9 +3,6 @@ package modulos;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import pages.ListProductsPage;
-import pages.LoginPage;
-import pages.ProductPage;
 import utility.GlobalVariables;
 
 import java.time.Duration;
@@ -21,55 +18,17 @@ public class ProductTest {
         browser.manage().window().maximize();
         browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         browser.get(GlobalVariables.base_url);
-
-        new LoginPage(browser)
-        .submitLoginSuccess(GlobalVariables.standard_user, GlobalVariables.password);
     }
 
     @Test
-    @DisplayName("Order by high price")
-    public void orderByHighPrice() {
-        ListProductsPage productsPage = new ListProductsPage(browser).selectProductSort("hilo");
+    @DisplayName("search a product")
+    public void searchProduct(){
 
-        Double highPrice = Double.parseDouble(
-                productsPage.getPriceElementByIndex(1).replaceAll("\\$\\d{0}", ""));
-
-        Double lowPrice = Double.parseDouble(
-                productsPage.getPriceElementByIndex(2).replaceAll("\\$\\d{0}", ""));
-
-        Assertions.assertTrue(highPrice > lowPrice);
-    }
-
-    @Test
-    @DisplayName("Order by low price")
-    public void orderByLowPrice() {
-        ListProductsPage productsPage = new ListProductsPage(browser).selectProductSort("lohi");
-
-        Double lowPrice = Double.parseDouble(
-                productsPage.getPriceElementByIndex(1).replaceAll("\\$\\d{0}", ""));
-
-        Double highPrice = Double.parseDouble(
-                productsPage.getPriceElementByIndex(2).replaceAll("\\$\\d{0}", ""));
-
-        Assertions.assertTrue(lowPrice < highPrice);
-    }
-
-    @Test
-    @DisplayName("Click in a product")
-    public void clickInAProduct(){
-        ListProductsPage productsPage = new ListProductsPage(browser);
-
-        String nameInListProductPage = productsPage.getNameFirstProduct();
-        productsPage.clickFirstProduct();
-
-        String nameInProductPage = new ProductPage(browser).getNameProduct();
-
-        Assertions.assertEquals(nameInListProductPage, nameInProductPage);
     }
 
     @AfterEach
     public void afterEach(){
-        browser.quit();
+        //browser.quit();
     }
 
 }
--- src/test/java/utility/GlobalVariables.java
+++ src/test/java/utility/GlobalVariables.java
@@ -3,8 +3,5 @@ package utility;
 public class GlobalVariables {
 
     public static final String path_chrome_driver = "C:\\Drivers\\chromedriver108\\chromedriver.exe";
-    public static final String base_url = "https://www.saucedemo.com/";
-    public static final String standard_user = "standard_user";
-    public static final String locked_out_user = "locked_out_user";
-    public static final String password = "secret_sauce";
+    public static final String base_url = "http://automationpractice.pl/index.php";
 }
